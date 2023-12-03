038b1a410b26567b12c57810f5ff1615fba77e9a
clb168 <clb168@case.edu>
2023-12-03 16:58:54 -0600

    changed QuestionGen

--- langAndOllama/langchainRag/QuestionGen.py
+++ langAndOllama/langchainRag/QuestionGen.py
@@ -10,7 +10,8 @@ llm = Ollama(model="mistral")
 loader = PyPDFLoader("/home/caden/ragExperiment/langAndOllama/langchainRag/exampleData/polaris_3.pdf")
 chain = QAGenerationChain.from_llm(llm=llm, text_splitter=text_splitter)
 
-data = loader.load()[0]
+data = loader.load()[100] #this chunck size and overlap seems to give 113 pages. -1 is referring to the last page
+#the pdf is 113 pages
 qa = chain.run(data.page_content)
-
+#print(data)
 print(qa)
