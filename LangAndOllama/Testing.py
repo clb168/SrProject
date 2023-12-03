d99ee180e385833587aacef786b518b7819fa0f8
clb168 <clb168@case.edu>
2023-12-02 21:41:17 -0600

    starting the build of rag using langchain + ollama

--- LangAndOllama/Testing.py
+++ LangAndOllama/Testing.py
@@ -2,5 +2,5 @@ from langchain.llms import Ollama
 llama2 = "llama2"
 llm = Ollama(base_url="http://127.0.0.1:11434", model=llama2)
 #llm = Ollama(model="llama2")
-res = llm.predict("hello")
+res = llm.predict("Tell me about supercomputers")
 print (res)
